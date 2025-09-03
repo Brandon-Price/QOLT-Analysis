aacd8aaa205532aae7114754de62cf82a040149d
Brandon Price <brandon.samuel.price@gmail.com>
2025-09-02 23:32:32 -0500

    Create web scrape script and pass data to notebook to join

--- web_scrape.py
+++ web_scrape.py
@@ -1,10 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
-import requests
+import re
+import time
 
-# Not all countries are available from this source
+options = Options()
+options.add_argument("--headless=new")
+driver = webdriver.Chrome(options=options)
 
-response = requests.get(
-    'https://codesubmit.io/blog/software-engineer-salary-by-country/'
-)
+driver.get("https://codesubmit.io/blog/software-engineer-salary-by-country/")
+time.sleep(3)
 
-print(response.status_code)
+soup = BeautifulSoup(driver.page_source, "html.parser")
+
+salary_pattern = re.compile(r"\$\s?[\d,]+")
+results = []
+
+for img in soup.find_all("img", title=True):
+    country = img["title"].strip()
+    salary_tag = img.find_next(string=salary_pattern)
+    if salary_tag:
+        salary = salary_pattern.search(salary_tag).group()
+        results.append((country, salary))
+
+driver.quit()
